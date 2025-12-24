c533e1233cf279f9e01392dbef904b95e5677462
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2025-12-23 22:18:47 -0500

    Testing how to fetch live and recent news to feed to news sentiment LLM

---
+++ backend/fetchnews.py
@@ -0,0 +1,36 @@
+from newsapi import NewsApiClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("NEWS_API_KEY")
+
+newsapi = NewsApiClient(API_KEY)
+
+top_headlines = newsapi.get_top_headlines(
+    q="Tech",
+    country="us",
+    category="business",
+    page_size=10
+)
+
+articles = newsapi.get_everything(
+    q="Nvidia",
+    language="en",
+    sort_by="publishedAt",
+    page_size=10
+
+)
+headlines = top_headlines["articles"]
+list_of_headlines = []
+for headline in headlines:
+    list_of_headlines.append(headline["title"])
+
+for headline in list_of_headlines:
+    print(headline)
+    print("-"*25)
+
+#print(top_headlines["articles"][0]["title"])
+#print(top_headlines)
+#print(articles["totalResults"])
+#print(articles)

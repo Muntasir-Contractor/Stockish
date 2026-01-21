621b454f561d2f2536ccbc77d5564998684f67d6
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-01-20 20:13:29 -0500

    Implementing Database

---
+++ Newsentiment.session.sql
@@ -0,0 +1,7 @@
+CREATE TABLE stock_info(
+    ticker varchar(4) PRIMARY KEY,
+    scalar float NOT NULL,
+    date_stamp date
+
+);
+
--- backend/newsentiment.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE stock_info(
-    ticker varchar(4) PRIMARY KEY,
-    sentiment_score float NOT NULL,
-    date_entered DATE
-);

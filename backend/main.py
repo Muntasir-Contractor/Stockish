d3007529668a971f7ed6583abc8b39dcd250bf6e
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-01-28 17:19:52 -0500

    Add CORS middleware to FastAPI app
    Configured CORS to allow requests from http://localhost:3000, enabling cross-origin requests for development purposes.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000"
+
+]
+app.add_middleware(
+    CORSMiddleware=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 #Get for reading
 #Post for create

19a0e61dc1308904d65f8037ebe56aff03fc7fa0
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-01-28 17:14:16 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Get for reading
+#Post for create
+#Put for update
+#Delete for delete
+
 #Start of a beginning
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+@app.get("/stock/{ticker}")
+def get_stock_data(ticker : str):
+    pass
+
+@app.get("/homepage")
+def get_homepage_data():
+    pass

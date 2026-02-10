093ae967f2eb357fb6634d8e44e109c1d54784d7
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-02-10 10:54:37 -0500

    API connection and fetching
    Got front-end connecting with api to make dynamic changes to website

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from topmovers import get_top_movers
 
 app = FastAPI()
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://localhost:5174"
 
 ]
 app.add_middleware(
-    CORSMiddleware=origins,
+    CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -27,6 +30,12 @@ def root():
 def get_stock_data(ticker : str):
     pass
 
+@app.get("/topmovers")
+def top_movers():
+    return get_top_movers()
+
 @app.get("/homepage")
 def get_homepage_data():
     pass
+
+#uvicorn main:app --reload
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import api from "./components/api.js";
 // useState lets you create state variables
   // useEffect lets you run code in response to events, such as when the first compnent renders, or state changes
 
@@ -9,18 +10,33 @@ function App(){
   // Declare a staste viarable data with initial value null
   // setData is the function used to updata 'data'
   //const {ticker} = useParams(); 
-  const [data, setData] = useState(null);
+  const [topmovers, setTopMovers] = useState([]);
+  console.log(api)
 
+  useEffect(() => {
+    api.get("/topmovers")
+      .then(res => {
+        setTopMovers(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
   return (
     <div>
     <div className="Navbar">
       <h1 >Hello world</h1>
     </div>
     <div>
-      <p> This is a paragraph</p>
+      <ul>{topmovers.map(u => (
+        <li key={u.symbol}>{u.symbol} ({u.name}): ${u.price}USD <ul>
+          <li> {u.change} , {u.changesPercentage}%</li>
+          </ul></li>
+
+      ))}</ul>
     </div>
     </div>
-  )
+  );
 }
 
 export default App

5bd88996fe7247604d8ae7a5437514be8d66d986
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-02-24 13:32:26 -0500

    Update Newsentiment.session.sql

--- Newsentiment.session.sql
+++ Newsentiment.session.sql
@@ -1,12 +1,1 @@
-CREATE TABLE stock_sentiment(
-    ticker TEXT NOT NULL,
-    scalar FLOAT NOT NULL,
-    raw_json TEXT NOT NULL,
-    overall_sentiment TEXT,
-    bullish_count INTEGER,
-    bearish_count INTEGER,
-    neutral_count INTEGER,
-    token_usage INTEGER,
-    created_at TEXT DEFAULT (datetime('now'))
-
-)
+ALTER TABLE stock_sentiment ADD COLUMN date_stamp DATE DEFAULT CURRENT_TIMESTAMP;

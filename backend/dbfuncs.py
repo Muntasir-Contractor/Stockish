9c46a3168426e43f95234660f881298f9630f376
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-02-25 19:33:02 -0500

    Updating db functions
    Created a new table that takes in more coloumns, adjusted db functions accordingly. Added new safety netting with "with sqlite3.connection(Connection) as conn" Closing connection automatically

--- backend/dbfuncs.py
+++ backend/dbfuncs.py
@@ -1,59 +1,50 @@
 import sqlite3
 from datetime import date , datetime
+import json
 
 CONNECTION = "newsentiment.db"
 
+#### DOUBLE-OPENING CONNECTION WITH EXISTS_IN_DB AND OTHER FUNCTIONS
+#### OPTIMIZE LATER
+
 
 def exists_in_db(ticker : str) -> bool:
-    conn = sqlite3.connect(CONNECTION)
-    cursor = conn.cursor()
-    #If the ticker pops up in the database return true
-    cursor.execute("SELECT * FROM stock_info WHERE ticker = ?" , (ticker,))
-    stock_info = cursor.fetchone()
-    #If the ticker does not exist in data return false
-    if stock_info == None:
-        return False
-    #Otherwise the ticker exists in data
-    return True
+    with sqlite3.connect(CONNECTION) as conn:
+        cursor = conn.cursor()
+        #If the ticker pops up in the database return true
+        cursor.execute("SELECT * FROM stock_sentiment WHERE ticker = ?" , (ticker,))
+        stock_info = cursor.fetchone()
+        #If the ticker does not exist in data return false
+        if stock_info == None:
+            return False
+        #Otherwise the ticker exists in data
+        return True
 
 #Always run exists_in_db before implementing this
 #If exists_in_db returns false then run this
-"""
-def insert_stock(ticker : str, scalar : float, notes = ""):
-    conn = sqlite3.connect(CONNECTION)
-    cursor = conn.cursor()
-    today = date.today()
-    date_stamp = today.strftime("%Y-%m-%d")
-    cursor.execute(
-        "INSERT INTO stock_sentiment (ticker, scalar, notes, date_stamp) VALUES (?, ?, ?, ?)",
-        (ticker, scalar, notes or None, date_stamp)
-    )
-    conn.commit()
-    conn.close()
-    """
 def insert_stock(ticker : str, scalar : float, insights,token_usage=None):
-    conn = sqlite3.connect(CONNECTION)
-    cursor = conn.cursor()
-    today = date.today()
-    date_stamp = today.strftime("%Y-%m-%d")
-    cursor.execute(
-        "INSERT INTO stock_sentiment (ticker, scalar, raw_json, date_stamp, token_usage) VALUES (?, ?, ?, ?, ?)",
-        (ticker, scalar, insights, date_stamp, token_usage)
-    )
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(CONNECTION) as conn:
+        cursor = conn.cursor()
+        today = date.today()
+        date_stamp = today.strftime("%Y-%m-%d")
+        cursor.execute(
+            "INSERT INTO stock_sentiment (ticker, scalar, raw_json, date_stamp, token_usage) VALUES (?, ?, ?, ?, ?)",
+            (ticker, scalar, insights, date_stamp, token_usage)
+        )
+        conn.commit()
 
 
 #gets the date stamp from ticker
 #runs exists_in_db before running this
 def get_date(ticker : str):
-    conn = sqlite3.connect(CONNECTION)
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT date_stamp FROM stock_sentiment WHERE ticker = ?" , (ticker,))
-    #Unpacking the date
-    date = (cursor.fetchone())[0]
-    conn.close()
-    return date
+    if not exists_in_db(ticker):
+        return None
+    with sqlite3.connect(CONNECTION) as conn:
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT date_stamp FROM stock_sentiment WHERE ticker = ?" , (ticker,))
+        #Unpacking the date
+        date_stamp = (cursor.fetchone())[0]
+        return date_stamp
 
 #returns The difference in days
 # Use .total_seconds() / 3600 to get hour difference
@@ -66,17 +57,28 @@ def date_difference(date_stamp : str):
     return (current_date - date_stamp)
 
 def get_scalar_from_db(ticker : str):
-    conn = sqlite3.connect(CONNECTION)
-    cursor = conn.cursor()
-    cursor.execute("SELECT scalar FROM stock_sentiment WHERE ticker = ?", (ticker,))
-    res = (cursor.fetchone())[0]
-    conn.close()
-    return res
+    with sqlite3.connect(CONNECTION) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT scalar FROM stock_sentiment WHERE ticker = ?", (ticker,))
+        res = (cursor.fetchone())[0]
+        return res
 
-def update_row(ticker : str, scalar, note=""):
-    conn = sqlite3.connect(CONNECTION)
-    cursor = conn.cursor()
-    cursor.execute("UPDATE stock_sentiment SET scalar = ? , raw_json = ? WHERE ticker = ?" , (scalar, None if not note else note, ticker))
-    conn.commit()
-    conn.exit()
+def get_insights_from_db(ticker : str):
+    if not exists_in_db(ticker):
+        return None
+    with sqlite3.connect(CONNECTION) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT scalar, raw_json FROM stock_sentiment WHERE ticker = ?", (ticker,))
+        tupl = cursor.fetchone()
+        scalar, insights = tupl
+        insights = json.loads(insights)
+        return scalar, insights
+
+def update_row(ticker : str, scalar, insights="", token_usage=0):
+    if not exists_in_db(ticker):
+        return None
+    with sqlite3.connect(CONNECTION) as conn:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE stock_sentiment SET scalar = ? , raw_json = ?, token_usage = ? WHERE ticker = ?" , (scalar, None if not insights else insights, token_usage, ticker))
+        conn.commit()
     return

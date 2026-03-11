0db52f82390ee8279b3f8bf915d9efb2a51fe63a
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-03-11 19:01:06 -0400

    Testing different parameters, different features, plotting feature importances, optimized raw accuracy of 13.78%

---
+++ scripts/train_forwardreturn_model.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import xgboost
+import joblib
+from sklearn.metrics import accuracy_score, classification_report
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('dataset/model_data_cleaned.csv')
+
+df['Date'] = pd.to_datetime(df['Date'])
+df = df.sort_values('Date')
+
+split_idx = int(len(df) * 0.8)
+# Everything BEFORE the 80% mark is Training
+train_df = df.iloc[:split_idx]
+# Everything AFTER the 80% mark is Testing
+test_df = df.iloc[split_idx:]
+
+print(f"Training rows (80%): {len(train_df)}")
+print(f"Testing rows (20%): {len(test_df)}")
+print(f"Data split at date: {train_df['Date'].max().date()}")
+print(f"Latest date in train: {test_df['Date'].max().date()}")
+
+
+features_to_drop = ['Ticker', 'Date', 'y_target'] + [col for col in train_df.columns if 'Sector_' in col]
+
+X_train = train_df.drop(columns=features_to_drop)
+y_train = train_df['y_target']
+
+X_test = test_df.drop(columns=features_to_drop)
+y_test = test_df['y_target']
+
+
+
+def train_test_model():
+    model = xgboost.XGBClassifier(
+        objective='multi:softmax',
+        num_class=10,
+        n_estimators=300,
+        max_depth=4,
+        learning_rate=0.15,
+        random_state=42,
+        n_jobs=-1)
+    model.fit(X_train,y_train)
+    print("Training Complete")
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test,y_pred)
+    print(f"Raw Accuracy: {accuracy*100:.2f}%")
+    print(classification_report(y_test,y_pred))
+    importances = model.feature_importances_
+
+# Bind them to the column names from your X_train dataset
+    feature_names = X_train.columns
+    importance_df = pd.DataFrame({
+        'Feature': feature_names,
+        'Importance': importances
+    })
+
+    # Sort them from most important to least important
+    importance_df = importance_df.sort_values(by='Importance', ascending=True)
+
+    # Plot the results
+    plt.figure(figsize=(10, 6))
+    plt.barh(importance_df['Feature'], importance_df['Importance'], color='skyblue')
+    plt.title('XGBoost Feature Importances (What drives the 1-Year Return?)')
+    plt.xlabel('Relative Importance')
+    plt.tight_layout()
+    plt.show()
+
+train_test_model()
+
+
+

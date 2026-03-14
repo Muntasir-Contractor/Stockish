3f927ff486a326908e8ec8a3e41863596a11ac2b
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-03-14 10:42:25 -0400

    Analyzed the confusion matrix of 1-year forward return model to see how "close enough" accurate the model was

--- scripts/fr_model_backtesting.py
+++ scripts/fr_model_backtesting.py
@@ -1,13 +1,26 @@
 import pandas as pd
 import joblib
 from train_forwardreturn_model import X_test,test_df, y_test
+from sklearn.metrics import confusion_matrix
+
+# GROUP DECILES BY LOCAL YEAR RATHER THAN GLOBAL DECILE SINCE DATASET CONTAINS YEARS FROM 2022-2024
+# 117 of 0's were predicting to be in 6.0-9.0 Range, minimize by adding additional features that spot growth traps
+
 
 model = joblib.load(r"model/XGBoost_fr_model.joblib")
+
+
 
 test_df = test_df.copy()
 test_df['Predicted_Decile'] = model.predict(X_test)
 
 print("Finished predicting")
+
+def confusionMatrix():
+    y_pred = test_df['Predicted_Decile']
+    matrix = confusion_matrix(y_test, y_pred,labels=list(range(10)))
+    print(matrix)
+
 
 portfolio = test_df[test_df['Predicted_Decile'] == 9]
 portfolio_return = portfolio['Forward_1yr_Return'].mean()
@@ -15,10 +28,10 @@ average_market_return = test_df['Forward_1yr_Return'].mean()
 portfolio_median = portfolio['Forward_1yr_Return'].median()
 min = portfolio['Forward_1yr_Return'].min()
 maxx = portfolio['Forward_1yr_Return'].max()
-
+"""
 for index,row in portfolio.iterrows():
     print(f"{row["Ticker"]} Bought, Predicted: {row["Predicted_Decile"]}, Actual Return: {row["Forward_1yr_Return"]*100:.2f}% Sector: {row["Sector_Basic"]}")
-
+"""
 print(f"Average Market Return (Benchmark):  {average_market_return * 100:.2f}%")
 print(f"Model's 'Strong Buy' Return:        {portfolio_return * 100:.2f}%  <-- (Did we beat the market?)")
 print(f"Median Return: {portfolio_median*100:.2f}")
@@ -29,3 +42,4 @@ print(f"Min Return: {min*100:.2f}")
 print("-" * 50)
 print(f"Number of Stocks Bought:  {len(portfolio)}")
 
+confusionMatrix()

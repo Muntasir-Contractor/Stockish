a08100d241cf371b65ac2e5025ed45a7575e52a8
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-03-27 21:49:18 -0400

    Revamping the get_ticker_news function to include summary of the article, so that the AI news sentiment analysis can give a more accurate scoring and encapsulation

--- backend/fetchnews.py
+++ backend/fetchnews.py
@@ -5,6 +5,22 @@ import yfinance as yf
 
 # FUTURE IMPLEMENTATION, fetch summary alongside title for better sentiment analysis, costs more tokens tho
 
+def get_ticker_newss(ticker):
+    stock = yf.Ticker(ticker)
+    news_data = stock.news
+    news = []
+    for idx in range(1,(min(len(news_data), 6))):
+        news.append({"Headline": news_data[idx]['content']['title'],
+                     "Summary": news_data[idx]['content']['summary']})
+        
+    return news
+
+def news_toString(news : list[dict]):
+    string_news = ""
+    for idx,news_data in enumerate(news):
+        string_news += f"{idx}. Headline: '{news_data["Headline"]}' \n Summary: '{news_data['Summary']}' \n \n"
+    
+    return string_news
 def get_ticker_news(ticker):
     """
     Get news for specific stocks via Yahoo Finance
@@ -24,52 +40,7 @@ def get_ticker_news(ticker):
     
     return res_prompt
 
-"""
-load_dotenv()
-finnhub_api_key = os.getenv("FINNHUB_KEY")
-def get_news(api_key=finnhub_api_key,category='general'):
-    finnhub_client = finnhub.Client(finnhub_api_key)
-    news = finnhub_client(category,min_id=0)
-    res_prompt = ""
-    for idx, article in enumerate(news[:10]):
-        res_prompt += f"{idx+1}. {article['headline']}"
-    
-    return res_prompt
-
-"""
-
-"""
-load_dotenv()
-API_KEY = os.getenv("NEWS_API_KEY")
-
-newsapi = NewsApiClient(API_KEY)
-
-top_headlines = newsapi.get_top_headlines(
-    country="us",
-    category="business",
-    page_size=10
-)
-
-articles = newsapi.get_everything(
-    q="Nvidia",
-    language="en",
-    sort_by="publishedAt",
-    page_size=10
-
-)
-headlines = top_headlines["articles"]
-list_of_headlines = []
-for headline in headlines:
-    list_of_headlines.append(headline["title"])
-
-for headline in list_of_headlines:
-    print(headline)
-    print("-"*25)
-
-#print(top_headlines["articles"][0]["title"])
-#print(top_headlines)
-#print(articles["totalResults"])
-#print(articles)
-
-#...
-"""
+if __name__ == "__main__":
+    news = get_ticker_newss("NVDA")
+    new_news = news_toString(news)
+    print(new_news)
